c1a47f70ba324402bd5e23ba3719b97e8019e5a2
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2025-03-13 10:47:47 +0200

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1pfYusCu8bS4jknbb9XGg7aAXsU1gk6Er/view?usp=sharing"
+          href="https://drive.google.com/file/d/16L18qrE2vNpD2BwyOAyILYCC8INKijoV/view?usp=sharing"
           download
         >
           Download CV{" "}
